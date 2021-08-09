ce91ee6979b7168b623b7170f0d491f7b861557c
Shivang Dubey <shivangdubey8@gmail.com>
2021-08-09 18:22:54 +0530

    Adding problems

---
+++ leetcode/shivang/Steps_to_reduce_number_to_zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int count = 0;
+        for ( ; num > 0; count++)
+        {
+            if (num % 2 == 0)
+            {
+                num = num / 2;
+            }
+            else
+            {
+                num = num - 1;
+            }
+        }
+        return count;
+
+    }
+};
