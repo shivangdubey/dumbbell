ed20347c96dc74c23a9113f2f71f5c4b6fa95a5f
Shivang Dubey <shivangdubey8@gmail.com>
2021-08-13 16:29:51 +0530

    263.Ugly Number

--- leetcode/shivang/maths/263. Ugly Number.cpp
+++ leetcode/shivang/maths/263. Ugly Number.cpp
@@ -6,17 +6,17 @@ public:
 
         while (n % 2 == 0)
         {
-            n /= 2;
+            n = n / 2;
         }
 
         while (n % 3 == 0)
         {
-            n /= 3;
+            n = n / 3;
         }
 
         while (n % 5 == 0)
         {
-            n /= 5;
+            n = n / 5;
         }
 
         return n == 1;
